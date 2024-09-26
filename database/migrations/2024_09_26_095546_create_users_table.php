4395b8bf2cc275347925e3ce1852d5f2f285b6d5
vchandra22 <vincentchaandra@gmail.com>
2024-09-26 17:01:17 +0700

    feat(database): create database migration, models, cpntroller for users

---
+++ database/migrations/2024_09_26_095546_create_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string("name")->nullable(false);
+            $table->integer('age')->nullable(false);
+            $table->string("job_title")->nullable(false);
+            $table->string("company")->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
