fefd0845be71c9127aa23e734f0fd3f98d095508
vchandra22 <vincentchaandra@gmail.com>
2024-09-26 17:13:50 +0700

    feat(request): add UserCreateRequest for creating new users

---
+++ app/Http/Requests/UserCreateRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class UserCreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "name" => ['required', 'max:255', 'string'],
+            "age" => ['required', 'integer'],
+            "job_title" => ['required', 'max:255', 'string'],
+            "company" => ['required', 'max:255', 'string'],
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response([
+            "errors" => $validator->getMessageBag()
+        ], 400));
+    }
+}
